17b0c30830d1c967328c2871a2b89c01eba7f689
JD-Tech28 <jeevanbirwas693@gmail.com>
2026-04-16 17:54:05 +0530

    Update app.py

--- website/app.py
+++ website/app.py
@@ -30,5 +30,5 @@ def home():
     return render_template("index.html", message=message)
 
 if __name__=="__main__":
-    # app.run(debug=True)
+    app.run(debug=True)
     app.run(host="0.0.0.0", port=5000)
